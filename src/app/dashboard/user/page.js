6aacaec8d3a7cf54e5166084c92817529d2c0ffe
Fardin7864 <fardinhasan1888@gmail.com>
2024-03-11 01:25:11 +0600

    create heading of user page

--- src/app/dashboard/user/page.js
+++ src/app/dashboard/user/page.js
@@ -1,11 +1,27 @@
 import Home from '@/dashboardComponents/Home';
 import React from 'react';
+import { BsCaretDownFill, BsSearch } from 'react-icons/bs';
+import { MdDelete } from 'react-icons/md';
 
 const page = () => {
     return (
         <div className=" bg-gray-300">
-        <div className=" max-w-7xl mx-auto pt-2 px-5">
+        <div className=" max-w-[1440px] mx-auto pt-2 px-5 flex gap-10">
             <Home />
+            <div className="  w-full bg-white rounded-lg">
+                <div className=' flex justify-between w-full border-b pb-4 p-4'>
+                <h6 className=' text-blue-500 font-medium'>Manage Users</h6>
+                <p className=' font-medium'>Total Users {'(190)'} </p>
+                <div className=' flex gap-4'>
+                    <button className='bg-red-400 text-white px-4 rounded-md py-2 hover:opacity-65 active:opacity-40'>Both</button>
+                    <button className='bg-green-400 text-white px-4 rounded-md py-2 hover:opacity-65 active:opacity-40'>Seller</button>
+                    <button className='bg-green-400 text-white px-4 rounded-md py-2 hover:opacity-65 active:opacity-40'>customer</button>
+                    <button className='bg-red-400 text-white px-2 rounded-md py-2 flex items-center justify-center gap-1 hover:opacity-65 active:opacity-40'><MdDelete className=' text-xl'/><BsCaretDownFill /></button>
+                    <button className='bg-green-400 text-white px-2 rounded-md py-2 hover:opacity-65 active:opacity-40'><BsSearch className=' text-xl font-bold'/></button>
+                    <button className='bg-[#686dff] text-white px-4 rounded-md py-2 hover:opacity-65 active:opacity-40'>+ Create User</button>
+                </div>
+                </div>
+            </div>
         </div>
       </div>
     );
