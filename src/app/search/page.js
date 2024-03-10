e2ab788afc4e23d5585032e29371c9037465ceab
Fardin7864 <fardinhasan1888@gmail.com>
2024-03-10 11:31:20 +0600

    make mobile navbar

---
+++ src/app/search/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const search = () => {
+    return (
+        <div>
+            <h3>This is search</h3>
+        </div>
+    );
+};
+
+export default search;
--- src/common/MobileNavbar.js
+++ src/common/MobileNavbar.js
@@ -1,28 +1,68 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaHome } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
-
+import { FaCirclePlus } from "react-icons/fa6";
+import { IoCallOutline, IoChatbubblesOutline  } from "react-icons/io5";
 
 
 const MobileNavbar = () => {
-    return (
-        <div>
-            <div>
-                <div className=" flex gap-4 px-5 border-t-2 rounded-t-xl mb-1">
-                    <button className=" flex flex-col justify-center items-center bg-gray-200 px-3 rounded-b-2xl pt-2 pb-1 text-sm font-medium"><FaHome className=" text-2xl"/> HOME</button>
-                    <button><IoSearch/> SEARCH</button>
-                </div>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
-            </div>
-            <div>
-
-            </div>
+  const location = usePathname();
+  console.log(location);
+  return (
+    <div>
+      <div className=" flex justify-between w-[100vw] px-1 h-16">
+        {/* First half */}
+        <div className=" flex px-2 border-t-2 rounded-t-xl mb-1 bg-white">
+          <Link
+            href={"/"}
+            className={`flex flex-col justify-center items-center ${
+              location === "/" ? "bg-gray-200" : "bg-transparent"
+            }  px-3 rounded-b-2xl pt-2 pb-1 text-sm font-medium`}
+          >
+            <FaHome className=" text-2xl" /> HOME
+          </Link>
+          <Link
+            href={"/search"}
+            className={`flex flex-col justify-center items-center ${
+              location === "/search" ? "bg-gray-200" : "bg-transparent"
+            }  px-3 rounded-b-2xl pt-2 pb-1 text-sm font-medium`}
+          >
+            <IoSearch className=" text-2xl" /> SEARCH
+          </Link>
         </div>
-    );
+        {/* plus logo */} 
+        <div className=" border-b-2 p-2 mb-6 rounded-full">
+        <FaCirclePlus className=" text-6xl text-[#fd6c16]"/>
+        </div>
+        {/* 2nd Half */}
+        <div className=" flex px-2 border-t-2 rounded-t-xl mb-1 bg-white">
+          <Link
+            href={"/"}
+            className={`flex flex-col justify-center items-center ${
+              location === "/" ? "bg-gray-200" : "bg-transparent"
+            }  px-3 rounded-b-2xl pt-2 pb-1 text-sm font-medium`}
+          >
+            <FaHome className=" text-2xl" /> HOME
+          </Link>
+          <Link
+            href={"/search"}
+            className={`flex flex-col justify-center items-center ${
+              location === "/search" ? "bg-gray-200" : "bg-transparent"
+            }  px-3 rounded-b-2xl pt-2 pb-1 text-sm font-medium`}
+          >
+            <IoSearch className=" text-2xl" /> SEARCH
+          </Link>
+        </div>
+      </div>
+      {/* Mobile foote */}
+      <div className="flex items-center justify-center gap-[2.5px]">
+        <button className=" w-1/2 py-4 flex items-center gap-1 bg-[#fd6c16] text-white justify-center rounded-bl-xl"><IoCallOutline/>Call</button>
+        <button  className=" w-1/2 py-4 flex items-center gap-1 bg-[#fd6c16] text-white justify-center rounded-br-xl"><IoChatbubblesOutline/>Chat</button>
+      </div>
+    </div>
+  );
 };
 
 export default MobileNavbar;
