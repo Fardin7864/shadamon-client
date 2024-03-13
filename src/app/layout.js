10cafe4ae60bf45219bfe6b679ac41e6c938f0ee
Fardin7864 <fardinhasan1888@gmail.com>
2024-03-14 00:30:49 +0600

    create user on mongodb

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,8 @@ import Navbar from "@/common/Navbar";
 import MobileNavbar from "@/common/MobileNavbar";
 import AuthanticationProvider from "@/common/AuthanticationContext";
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/common/SocialLogin.js
+++ src/common/SocialLogin.js
@@ -1,32 +1,68 @@
-"use client"
-import { useContext } from "react";
+"use client";
+import { useContext, useState } from "react";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { IoMail } from "react-icons/io5";
 import { AuthContext } from "./AuthanticationContext";
 
-
 const SocialLogin = () => {
-    const {loginWithProvider, google, successToast, faildToast, user} = useContext(AuthContext);
-    const handlePopup = (provider) => { 
-        loginWithProvider(provider)
-        .then(() => { 
-            navigate(location?.state ? location.state : "/")
-            successToast()
-            console.log(user)
-         })
-        .catch(() => {
-            faildToast()})
-     }
-     console.log(user)
-    return (
-        <div className=" px-10">
-            <button style={{backgroundColor: "#4267a6"}} className=" flex items-center gap-2 rounded-md py-2 px-5 w-full text-white hover:opacity-65 active:opacity-30"><FaFacebookSquare className=" text-white text-xl"/> Continue with Facebook</button>
-            {/* google */}
-            <button onClick={() => handlePopup(google)} style={{backgroundColor: "white", marginTop: '8px'}}  className=" flex items-center gap-2 rounded-md py-2 px-5 w-full border hover:opacity-65 active:opacity-30"><FcGoogle className=" text-white text-xl"/> Continue with Google</button>
-            <button style={{backgroundColor: "white", marginTop: '8px'}} className=" flex items-center gap-2 rounded-md py-2 px-5 w-full hover:opacity-65 active:opacity-30"><IoMail className=" text-xl"/> Continue with email</button>
-        </div>
-    );
+    const [logdInUser, setLogdInUser] = useState();
+  const { loginWithProvider, google, successToast, faildToast, user } =
+    useContext(AuthContext);
+  const handlePopup = (provider) => {
+    loginWithProvider(provider)
+      .then(() => {
+        // console.log(user);
+        const newUser = {
+          username: user?.displayName,
+          email: user?.email,
+          roll: "user",
+          password: "Test123",
+        };
+        // conso////////le.log(newUser);
+        const registerUser = fetch("http://localhost:8000/api/v1/users/register", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newUser),
+        })
+        .then(response => response.json())
+        .then(data => setLogdInUser(data))
+
+        // console.log(registerUser);
+        successToast();
+      })
+      .catch(() => {
+        faildToast();
+      });
+  };
+  //  console.log(user?.displayName)
+  return (
+    <div className=" px-10">
+      <button
+        style={{ backgroundColor: "#4267a6" }}
+        className=" flex items-center gap-2 rounded-md py-2 px-5 w-full text-white hover:opacity-65 active:opacity-30"
+      >
+        <FaFacebookSquare className=" text-white text-xl" /> Continue with
+        Facebook
+      </button>
+      {/* google */}
+      <button
+        onClick={() => handlePopup(google)}
+        style={{ backgroundColor: "white", marginTop: "8px" }}
+        className=" flex items-center gap-2 rounded-md py-2 px-5 w-full border hover:opacity-65 active:opacity-30"
+      >
+        <FcGoogle className=" text-white text-xl" /> Continue with Google
+      </button>
+      <button
+        style={{ backgroundColor: "white", marginTop: "8px" }}
+        className=" flex items-center gap-2 rounded-md py-2 px-5 w-full hover:opacity-65 active:opacity-30"
+      >
+        <IoMail className=" text-xl" /> Continue with email
+      </button>
+    </div>
+  );
 };
 
 export default SocialLogin;
