34347830d3e44144d29234941614bfe8611959b0
Afsana Meem <afsumeem@gmail.com>
2024-03-21 13:32:26 +0600

    detail page done

--- src/app/details/page.js
+++ src/app/details/page.js
@@ -8,6 +8,12 @@ import { FaTruck } from "react-icons/fa";
 import { RiArrowGoBackFill } from "react-icons/ri";
 import { SlClose } from "react-icons/sl";
 import { FaRegArrowAltCircleUp } from "react-icons/fa";
+import { LiaArrowCircleUpSolid } from "react-icons/lia";
+import { IoLocation } from "react-icons/io5";
+import { SlCalender } from "react-icons/sl";
+import { FaHeartCirclePlus } from "react-icons/fa6";
+import { FaShareAltSquare } from "react-icons/fa";
+import { FaExclamationCircle } from "react-icons/fa";
 
 const Details = () => {
   return (
@@ -92,7 +98,7 @@ const Details = () => {
             </button>
           </div>
           <div className="mb-2">
-            <h2 className="text-slate-600">
+            <h2 className="text-slate-600 text-sm">
               Condition: Used, <span>Brand: Hero, </span>
               <span>Delivery: Home Delivery, </span>{" "}
               <span>Register: 2014, </span>
@@ -106,7 +112,7 @@ const Details = () => {
 
           <div className="mt-3 mb-2">
             <h2 className="font-bold">Description</h2>
-            <h2 className="text-slate-600">
+            <h2 className="text-slate-600 text-sm">
               - Intel H110 Express Chipset - Intel G4560 (7tel G4560(7th Gen),
               Clock Speed 3.50GHz (3MB Cache ... <span>See More</span> ))
             </h2>
@@ -125,25 +131,73 @@ const Details = () => {
               <FaAngleUp />
             </button>
             <div className="mt-3">
-              <h2 className="flex gap-2 items-center">
+              <h2 className="flex gap-2 items-center text-slate-700 text-sm">
                 <FaTruck /> This Merchant can ship to BD
               </h2>
-              <h2>
+              <h2 className="flex gap-2 items-center text-slate-700 text-sm">
                 <RiArrowGoBackFill />
                 You can return new and unused items used items within 30 days
               </h2>
-              <h2>
-                {" "}
+              <h2 className="flex gap-2 items-center text-slate-700 text-sm">
                 <SlClose /> You can cancel within 30 minutes of of purchase
               </h2>
 
-              <button>
+              <button className="flex gap-2 items-center text-slate-700 text-sm">
                 <FaRegArrowAltCircleUp />
                 Promote This Post
               </button>
             </div>
           </div>
+
+          <button className="border-2 bg-[#ffded0] text-[#fb6320] border-[#fb6320] w-full justify-center items-center gap-2 my-3 py-2 rounded-md font-bold flex ">
+            <span className="text-xl">
+              <LiaArrowCircleUpSolid />
+            </span>{" "}
+            Promote This Post
+          </button>
+
           <hr />
+
+          <div className="my-4 flex justify-center items-center gap-2">
+            <FaExclamationCircle className="text-2xl" />
+            <FaHeartCirclePlus className="text-3xl" />
+            <button className="bg-slate-900 text-white py-2 px-3 rounded-l-2xl rounded-r-2xl">
+              Close Detail
+            </button>
+            <FaShareAltSquare className="text-3xl rounded-full" />
+          </div>
+
+          <hr />
+        </div>
+      </div>
+      {/*  */}
+      <div className="mx-3 mt-3">
+        <h3 className="font-bold mb-2">Similar Product</h3>
+        <div>
+          <div className="flex gap-3 rounded-lg bg-white border-2 mb-5">
+            <img
+              className="h-[100px] w-[130px] rounded-lg"
+              src="https://shadamon.vercel.app/assets/premium-cover-pic.jpg"
+              alt=""
+            />
+            <div>
+              <h2 className="font-semibold text-md mt-1">
+                Oxygen Taipei NTU Oxygen Gongguan
+              </h2>
+              <h2 className="font-semibold text-md -mt-2">Taipei NTU Oxygen</h2>
+              <div className="flex gap-3">
+                <div className="flex items-center gap-1 text-slate-600 text-xs">
+                  <IoLocation />
+                  <p>Dhaka</p>
+                </div>
+                <div className="flex items-center gap-1 text-slate-600 text-xs">
+                  <SlCalender />
+                  <p>Dhaka</p>
+                </div>
+              </div>
+              <h4 className="font-semibold">$4959</h4>
+            </div>
+          </div>
         </div>
       </div>
     </div>
